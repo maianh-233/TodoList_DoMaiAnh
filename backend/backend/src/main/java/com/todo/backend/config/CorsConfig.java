0756cafc2e0c70ef48c7dd6184aacb5bc6e398e5
maianh-233 <162794036+maianh-233@users.noreply.github.com>
2026-07-07 10:50:46 +0700

    Fix backend

--- backend/backend/src/main/java/com/todo/backend/config/CorsConfig.java
+++ backend/backend/src/main/java/com/todo/backend/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://todolistdomaianh.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*");
     }
